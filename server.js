d56636d48cec9c19a49b9ed520e4d3478697f05b
Neeraj <neeraj_raj2@yahoo.co.in>
2018-09-19 23:00:31 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partial');
+hbs.registerHelper('getCurrentYear',()=>{
+return new Date().getFullYear();
+})
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    console.log(`${now}: ${req.method} ${req.url}`);
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    fs.appendFile('server.log', log + '\n',(err)=>{
+        if(err){
+            console.log('unable to write in file.');
+        }
+    })
+
+next();
+})
+
+app.get('/',(req, res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page'
+    });
+});
+
+app.get('/about',(req, res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page'
+    });
+});
+
+
+app.listen(2000);
